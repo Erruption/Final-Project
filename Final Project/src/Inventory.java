e22d2e7d6c1fe3d61214c4826dacd45f6c7cc580
91000AGN - Nicholas Agnew <91000AGN@WCDSB.CA>
2018-01-16 14:02:44 -0500

    whyyy

--- Final Project/src/Inventory.java
+++ Final Project/src/Inventory.java
@@ -1,4 +1,28 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class Inventory {
-//teeeest
+public class Inventory extends JFrame{
+	
+	public Inventory(){
+		super();
+		
+		
+		Container Pane = getContentPane();
+		Pane.setLayout(null);
+
+		//Frame
+		setTitle("Inventory");
+		setSize(350,135);
+		setResizable(false);
+		setLocation(200,200);
+		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+	
+	//testing purposes only
+	public static void main (String [] args){
+		new Inventory();
+	}
+
 }
