40a197456700666bc0eaefe61423d734fad2e922
91000AGN - Nicholas Agnew <91000AGN@WCDSB.CA>
2018-01-16 14:20:25 -0500

    inv

--- Final Project/src/Inventory.java
+++ Final Project/src/Inventory.java
@@ -6,16 +6,19 @@ public class Inventory extends JFrame{
 	public Inventory(){
 		super();
 		
-		
+		JLabel background = new JLabel(new ImageIcon("inventory-grid.png"));
+		setContentPane(background);
 		Container Pane = getContentPane();
 		Pane.setLayout(null);
-
+		
 		//Frame
 		setTitle("Inventory");
-		setSize(350,135);
+		setSize(520,250);
 		setResizable(false);
 		setLocation(200,200);
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
+		setVisible(true);
 		
 		
 	}
