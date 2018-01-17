384fda2f70489b671aafc15279a6b5562503ace9
91000AGN - Nicholas Agnew <91000AGN@WCDSB.CA>
2018-01-17 14:15:08 -0500

    Inventory update

--- Final Project/src/Inventory.java
+++ Final Project/src/Inventory.java
@@ -1,9 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Inventory extends JFrame{
 	
+	
 	public Inventory(){
+		
 		super();
 		
 		JLabel background = new JLabel(new ImageIcon("Resources/inventory-grid.png"));
@@ -13,19 +16,50 @@ public class Inventory extends JFrame{
 		
 		//Frame
 		setTitle("Inventory");
-		setSize(520,250);
+		setSize(517,328);
 		setResizable(false);
 		setLocation(200,200);
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		
+		//Resume Button (Resumes Game)
+		JButton btnResume = new JButton("Resume");
+		btnResume.setBounds(7, 231, 244, 60);
+		btnResume.setFont(new Font("Dialog", Font.BOLD, 20));
+		btnResume.addActionListener(new btnListener()); 
+		btnResume.setActionCommand("Resume");
+		Pane.add(btnResume);
+		
+		//Quit Button (Return to main menu) not implemented
+		JButton btnQuit = new JButton("Return to Menu");
+		btnQuit.setBounds(262, 231, 240, 60);
+		btnQuit.setFont(new Font("Dialog", Font.BOLD, 20));
+		btnQuit.addActionListener(new btnListener()); 
+		btnQuit.setActionCommand("Quit");
+		Pane.add(btnQuit);
+		
+
 		setVisible(true);
 		
 		
+	}
+	
+	private class btnListener implements ActionListener {
+
+		public void actionPerformed (ActionEvent e){
+
+			if (e.getActionCommand().equals("Resume")){ //Checks for an action from btnResume
+
+		
+			}
+			else if (e.getActionCommand().equals("Quit")) { //Checks for an action from btnQuit
+
+				
+			}
+		}
 	}
 	
 	//testing purposes only
 	public static void main (String [] args){
 		new Inventory();
 	}
-
 }
