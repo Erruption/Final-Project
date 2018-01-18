47d609c6738b71f2e8b61e67ed419dbc28b4f2ff
unknown <lzettler@golden.net>
2018-01-18 11:19:49 -0500

    spaghetti'd Jesh's code into the main

--- Final Project/src/Menu.java
+++ Final Project/src/Menu.java
@@ -1,4 +1,44 @@
 
-public class Menu {
+//public class Menu {
+	//MAKE THIS NEW MAIN SO IT STARTS BEFORE GUI DOES
+	import javax.swing.*;
+	public class Menu extends JFrame{
+		//hello is this working
+		public Menu () {
 
-}
+			JLabel background = new JLabel(new ImageIcon("back.jpg"));
+			setContentPane(background);
+			setTitle("Game");
+			setSize(400,400);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			setResizable(false);
+			Container cp = getContentPane();
+			cp.setLayout(null); 
+			JButton Start = new JButton("Start");
+			Start.setActionCommand("Start");
+			JButton Exit = new JButton("Exit");
+			Exit.setActionCommand("Exit");
+			start.setBounds(100, 300, 20, 20);
+			exit.setBounds(300, 300, 20, 20);
+			add(start);
+			add(exit);
+			setVisible (true);
+
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			if ("Exit".equals(e.getActionCommand())) {
+				System.exit();
+			}
+			if ("Start".equals(e.getActionCommand())) {
+				new GUI();
+				System.exit();
+			}
+
+			public static void main(String args[]){
+				new Menu();
+			}
+		}
+
+	}
