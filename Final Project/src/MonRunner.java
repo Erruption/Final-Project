7a37a15b560582960f3bbc759b5529f998fd06c2
kingfishko <kingfishko101@hotmail.com>
2018-01-27 12:01:01 -0500

    Monsters

--- Final Project/src/MonRunner.java
+++ Final Project/src/MonRunner.java
@@ -21,7 +21,7 @@ public class MonRunner extends Monster {
 
 	@Override
 	public void calcMove() {
-		double deltaX = ((Menu.G.player.x + Menu.G.player.play.getWidth()/2) - this.x + this.MonsterIcon.getWidth()/2);
+		double deltaX = (Menu.G.player.x + Menu.G.player.play.getWidth()/2) - (this.x + this.MonsterIcon.getWidth()/2);
 		double deltaY = (Menu.G.player.y + Menu.G.player.play.getHeight()/2) - (this.y + this.MonsterIcon.getHeight()/2);
 		double hyp = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
 		
